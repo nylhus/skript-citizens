435780c041a92381d3a5100f62b09c3903800395
ShaneBee <shanebolenback@me.com>
2023-08-09 19:20:27 -0700

    Types - add parser (for toString) and changeup docs a smidge

--- src/main/java/ca/nylhus/skriptcitizens/elements/classes/Types.java
+++ src/main/java/ca/nylhus/skriptcitizens/elements/classes/Types.java
@@ -1,17 +1,40 @@
 package ca.nylhus.skriptcitizens.elements.classes;
 
 import ch.njol.skript.classes.ClassInfo;
+import ch.njol.skript.classes.Parser;
+import ch.njol.skript.lang.ParseContext;
 import ch.njol.skript.registrations.Classes;
 import net.citizensnpcs.api.npc.NPC;
+import org.eclipse.jdt.annotation.NonNull;
 
 public class Types {
 
     static {
         Classes.registerClass(new ClassInfo<>(NPC.class, "npc")
                 .user("npcs?")
-                .name("npc")
-                .description("Citizens npcs")
+                .name("Citizens NPC")
+                .description("Represents a Citizens NPC.")
                 .examples("last spawned npc")
-                .since("1.0.0"));
+                .since("1.0.0")
+                .parser(new Parser<NPC>() {
+                    @SuppressWarnings("NullableProblems")
+                    @Override
+                    public boolean canParse(ParseContext context) {
+                        return false;
+                    }
+
+                    @Override
+                    @NonNull
+                    public String toString(NPC npc, int flags) {
+                        return "citizen with id " + npc.getId();
+                    }
+
+                    @Override
+                    @NonNull
+                    public String toVariableNameString(NPC npc) {
+                        return "citizen:" + npc.getId();
+                    }
+                }));
     }
+
 }
