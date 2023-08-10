6dd020960d7f30e518094b7fd1160d9eef8bb2e4
ShaneBee <shanebolenback@me.com>
2023-08-09 19:49:02 -0700

    Types - add changer to delete an NPC

--- src/main/java/ca/nylhus/skriptcitizens/elements/classes/Types.java
+++ src/main/java/ca/nylhus/skriptcitizens/elements/classes/Types.java
@@ -1,20 +1,27 @@
 package ca.nylhus.skriptcitizens.elements.classes;
 
+import ch.njol.skript.classes.Changer;
 import ch.njol.skript.classes.ClassInfo;
 import ch.njol.skript.classes.Parser;
 import ch.njol.skript.lang.ParseContext;
 import ch.njol.skript.registrations.Classes;
+import ch.njol.util.coll.CollectionUtils;
+import net.citizensnpcs.api.CitizensAPI;
 import net.citizensnpcs.api.npc.NPC;
+import net.citizensnpcs.api.npc.NPCRegistry;
 import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
 
 public class Types {
+
+    private static final NPCRegistry NPC_REGISTRY = CitizensAPI.getNPCRegistry();
 
     static {
         Classes.registerClass(new ClassInfo<>(NPC.class, "npc")
                 .user("npcs?")
                 .name("Citizens NPC")
                 .description("Represents a Citizens NPC.")
-                .examples("last spawned npc")
+                .examples("last spawned npc", "delete last spawned npc")
                 .since("1.0.0")
                 .parser(new Parser<NPC>() {
                     @SuppressWarnings("NullableProblems")
@@ -33,6 +40,24 @@ public class Types {
                     @NonNull
                     public String toVariableNameString(NPC npc) {
                         return "citizen:" + npc.getId();
+                    }
+                })
+                .changer(new Changer<NPC>() {
+                    @SuppressWarnings("NullableProblems")
+                    @Override
+                    public @Nullable Class<?>[] acceptChange(ChangeMode mode) {
+                        if (mode == ChangeMode.DELETE) return CollectionUtils.array();
+                        return null;
+                    }
+
+                    @SuppressWarnings("NullableProblems")
+                    @Override
+                    public void change(NPC[] what, @Nullable Object[] delta, ChangeMode mode) {
+                        if (mode == ChangeMode.DELETE) {
+                            for (NPC npc : what) {
+                                NPC_REGISTRY.deregister(npc);
+                            }
+                        }
                     }
                 }));
     }
