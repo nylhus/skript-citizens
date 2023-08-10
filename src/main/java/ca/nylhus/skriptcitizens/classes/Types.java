bb70b68982f488cd68aa9435e8a9a7fe4d26378b
ShaneBee <shanebolenback@me.com>
2023-08-10 10:08:57 -0700

    types - i hope this fixes this

--- src/main/java/ca/nylhus/skriptcitizens/classes/Types.java
+++ src/main/java/ca/nylhus/skriptcitizens/classes/Types.java
@@ -1,11 +1,14 @@
 package ca.nylhus.skriptcitizens.classes;
 
+import ch.njol.skript.classes.Changer;
 import ch.njol.skript.classes.ClassInfo;
 import ch.njol.skript.classes.Parser;
 import ch.njol.skript.lang.ParseContext;
 import ch.njol.skript.registrations.Classes;
+import ch.njol.util.coll.CollectionUtils;
 import net.citizensnpcs.api.npc.NPC;
 import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
 
 public class Types {
 
@@ -14,7 +17,7 @@ public class Types {
                 .user("npcs?")
                 .name("Citizens NPC")
                 .description("Represents a Citizens NPC.")
-                .examples("last spawned npc")
+                .examples("last spawned npc", "delete last spawned npc")
                 .since("1.0.0")
                 .parser(new Parser<NPC>() {
                     @SuppressWarnings("NullableProblems")
@@ -33,6 +36,24 @@ public class Types {
                     @NonNull
                     public String toVariableNameString(NPC npc) {
                         return "citizen:" + npc.getId();
+                    }
+                })
+                .changer(new Changer<NPC>() {
+                    @SuppressWarnings("NullableProblems")
+                    @Override
+                    public @Nullable Class<?>[] acceptChange(ChangeMode mode) {
+                        if (mode == ChangeMode.DELETE) return CollectionUtils.array();
+                        return null;
+                    }
+
+                    @SuppressWarnings("NullableProblems")
+                    @Override
+                    public void change(NPC[] what, @Nullable Object[] delta, ChangeMode mode) {
+                        if (mode == ChangeMode.DELETE) {
+                            for (NPC npc : what) {
+                                npc.destroy();
+                            }
+                        }
                     }
                 }));
     }
